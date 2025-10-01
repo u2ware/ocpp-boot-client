e4f7119d29726c27a8c78afd1064e5775affa09f
u2waremanager@gamil.com <u2waremanager@gamil.com>
2025-10-01 16:26:33 +0900

    ocpp-boot-client

--- src/main/java/io/u2ware/ocpp/client/OCPPConfig.java
+++ src/main/java/io/u2ware/ocpp/client/OCPPConfig.java
@@ -1,14 +1,31 @@
 package io.u2ware.ocpp.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.u2ware.ocpp.config.EnableOcppClient;
+import io.u2ware.ocpp.config.OcppAttributes;
 import io.u2ware.ocpp.OCPPVersion;
 
 @Configuration
 @EnableOcppClient(version = OCPPVersion.V2_1, uri="ws://localhost:8081/ocpp")
 // @EnableOcppClient(version = OCPPVersion.V2_0_1, uri="ws://localhost:8081/ocpp")
 // @EnableOcppClient(version = OCPPVersion.V1_6, uri="ws://localhost:8081/ocpp")
-public class OCPPConfig {
+public class OCPPConfig implements WebMvcConfigurer{
     
+
+    @Autowired
+    protected OcppAttributes ocppAttributes;
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        String console = ocppAttributes.getOcppBootConsole();
+        String viewName = String.format("redirect:%s", console);
+        registry.addViewController("/").setViewName(viewName);
+    }    
+
+
 }
